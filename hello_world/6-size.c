d6c9bd3a5ba6b5032665537ad10115971c463e96
Tnozone <maxnocquet@gmail.com>
2022-09-30 05:44:21 -0700

    type size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ float floatType;
 long int longintType;
 long long int longlongintType;
 char charType;
-printf("Size of a char: %z byte\(s\) \n", sizeof(charType));
-printf("Size of an int: %z byte\(s\) \n", sizeof(intType));
-printf("Size of a long int: %z byte\(s\) \n", sizeof(longintType));
-printf("Size of a long long int: %z byte\(s\) \n", sizeof(longlongintType));
-printf("Size of a float: %z byte\(s\) \n", sizeof(floatType));
+printf("Size of a char: %zu byte\(s\) \n", sizeof(charType));
+printf("Size of an int: %zu byte\(s\) \n", sizeof(intType));
+printf("Size of a long int: %zu byte\(s\) \n", sizeof(longintType));
+printf("Size of a long long int: %zu byte\(s\) \n", sizeof(longlongintType));
+printf("Size of a float: %zu byte\(s\) \n", sizeof(floatType));
 return (0);
 }
