8abaf905431311d49c55054b17164d7f033a0f88
Tnozone <maxnocquet@gmail.com>
2022-10-03 05:08:46 -0700

    numbers putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description - prints single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int dig;
+
+for (dig = 0; dig <= 9; dig++)
+{
+putchar(dig);
+}
+putchar('\n');
+return (0);
+}
