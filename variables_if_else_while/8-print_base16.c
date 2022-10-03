77338d8c21ebea0bb27ce6b89949992d2cbb4061
Tnozone <maxnocquet@gmail.com>
2022-10-03 06:50:01 -0700

    last tasks files

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description - prints alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dig = 0x;
+
+	for (dig = 0; dig <= 'f'; dig++)
+	putchar(%x dig + '0');
+	putchar('\n');
+	return (0);
+}
