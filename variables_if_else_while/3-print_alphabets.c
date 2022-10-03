de4376a545d1b4e5915fc6ded2fd5a80070a2b27
Tnozone <maxnocquet@gmail.com>
2022-10-03 04:13:10 -0700

    alphabets low and up

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description - prints alphabet lowercase and uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char al;
+
+for (al = 'a'; al <= 'z'; al++)
+{
+putchar(al);
+}
+for (al = 'A'; al <= 'Z'; al++)
+{
+ptuchar(al);
+}
+putchar('\n');
+return (0);
+}
