4111009cbc2c0a751c05916cf4df9f163ce90fab
Tnozone <maxnocquet@gmail.com>
2022-10-03 03:57:50 -0700

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description - prints alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char al;
+
+for (al = 'a'; al <= 'z'; al++)
+{
+putchar(al);
+}
+return (0);
+}
