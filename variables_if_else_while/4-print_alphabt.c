e978ab573bae12a7627e156734dfbb434be4d3d9
Tnozone <maxnocquet@gmail.com>
2022-10-03 04:32:47 -0700

    alphabet minus 2

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description - prints alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char al;
+
+for (al = 'a'; al <= 'z'; al++)
+{
+if (al == 'd'; al == 'q')
+{
+continue;
+}
+putchar(al);
+}
+putchar('\n');
+return (0);
