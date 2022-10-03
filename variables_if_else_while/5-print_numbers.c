7a55866ffe8c12e1dbe0accab275804cb6bbcb97
Tnozone <maxnocquet@gmail.com>
2022-10-03 04:42:30 -0700

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description - prints single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int dig;
+
+for (dig = '0'; al <= '9'; dig++)
+{
+printf("%i", dig);
+}
+putchar('\n');
+return (0);
+}
