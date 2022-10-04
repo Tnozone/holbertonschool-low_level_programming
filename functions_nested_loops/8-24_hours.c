dd557de24073714df418df840a90eede88bb66b2
Tnozone <maxnocquet@gmail.com>
2022-10-04 06:09:22 -0700

    24 hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * _islower - Entry point
+ * description - prints minutes of a day
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+int x;
+int h = 24;
+int m = 60;
+
+for (h in x)
+{
+for (m in h)
+{
+_putchar(h, ':', m);
+}
+}
+}
