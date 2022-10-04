c86db5cc95b2210816b7f6be9777f7213c884cab
Tnozone <maxnocquet@gmail.com>
2022-10-04 05:57:01 -0700

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,23 @@
 /**
  * print_last_digit - Entry point
  * description - print last digit
+ * @n: integer with digits
  * Return: Always 0 (Success)
  */
 
 int print_last_digit(int n)
 {
+if (n > 0)
+{
 n = n % 10;
 _putchar(n);
 return (n);
+}
+else
+{
+n = (-1) * n;
+n = n % 10;
+_putchar(n);
+return (n);
+}
 }
