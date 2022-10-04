27e9e4bccb008d005b4182d685c7f3994a972d6e
Tnozone <maxnocquet@gmail.com>
2022-10-04 05:26:37 -0700

    absolute

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -9,12 +9,12 @@ int _abs(int n)
 {
   if (n > 0)
     {
-      _putchar(n);
+      return (n);
     }
   else
     {
       n = (-1) * n;
-      _putchar(n);
+      return (n);
     }
   return (0);
 }
