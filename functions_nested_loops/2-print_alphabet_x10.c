38821e14ef2de558e8470d736a97ac9cf7578948
Tnozone <maxnocquet@gmail.com>
2022-10-04 02:26:03 -0700

    10 alphabets

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include"main.h"
 /**
  * main - Entry point
  * description - print alphabet
  * Return: Always 0 (Success)
  */
 
-int main(void)
+void print_alphabet_x10(void);
 {
 char alph;
 int i = 0;
