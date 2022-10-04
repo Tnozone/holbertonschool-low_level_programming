839073fb5e357e94c96d720efdccc3dbbc63cb27
Tnozone <maxnocquet@gmail.com>
2022-10-04 05:20:25 -0700

    absolute

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * _abs - Entry point
+ * description - checks if lowercase
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+  if (n > 0)
+    {
+      _putchar(n);
+    }
+  else
+    {
+      n * -1;
+      _putchar(n);
+    }
+  return (0);
+}
