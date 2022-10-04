63dc35523b53ac6d2558dd4b457b2094eaac391e
Tnozone <maxnocquet@gmail.com>
2022-10-04 03:20:52 -0700

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * _islower - Entry point
+ * description - print alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+char c;
+
+_putchar(islower(c));
+if (true)
+{
+return (1);
+}
+if (false)
+{
+return (0);
+}
+}
