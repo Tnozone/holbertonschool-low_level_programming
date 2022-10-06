7f08d659aa9683d9230dc80aea0b07fcf1e55e7f
Tnozone <maxnocquet@gmail.com>
2022-10-06 01:03:54 -0700

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,21 +10,17 @@ void print_to_98(int n)
 {
 if (n < 98)
 {
-for (n < 98; n++;)
-{
 _putchar(n + '0');
 _putchar(',');
 _putchar(' ');
-}
+n++;
 }
 else if (n > 98)
 {
-for (n > 98; n--;)
-{
 _putchar(n + '0');
 _putchar(',');
 _putchar(' ');
-}
+n--;
 }
 else
 {
