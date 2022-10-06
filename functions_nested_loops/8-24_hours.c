be2b738726c716244162bc99bd59550bc18a0ade
Tnozone <maxnocquet@gmail.com>
2022-10-06 00:33:39 -0700

    24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,35 +7,18 @@
 
 void jack_bauer(void)
 {
-int h1 = 0;
-int h2;
-int m1;
-int m2;
+int h1, m1;
 
-while (h1 <= 2)
+for (h1 = 0; h1 <= 24; h1++)
 {
-for (h2 = 0; h2 < 10;)
+for (m1 = 0; m1 < 60; m1++)
 {
-for (m1 = 0; m1 < 6;)
-{
-for (m2 = 0; m2 < 10;)
-{
-if (h1 == 2 && h2 == 4)
-{
-break;
-}
-_putchar(h1 + '0');
-_putchar(h2 + '0');
+_putchar((h1 / 10) + '0');
+_putchar((h1 % 10) '0');
 _putchar(':');
-_putchar(m1 + '0');
-_putchar(m2 + '0');
+_putchar((m1 / 10) + '0');
+_putchar((m1 % 10) '0');
 _putchar('\n');
-m2++;
 }
-m1++;
-}
-h2++;
-}
-h1++;
 }
 }
