535e8ba143afe53cb1253fc06754cbfe18966908
Tnozone <maxnocquet@gmail.com>
2022-10-06 01:55:18 -0700

    9x

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,12 +9,12 @@ void times_table(void)
 {
 int x, y, z;
 
-for (x = 10; x <= 9; x++;)
+for (x = 10; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
 {
 z = x * y;
-if (y == 9);
+if (y == 9)
 {
 _putchar(z + '0');
 }
