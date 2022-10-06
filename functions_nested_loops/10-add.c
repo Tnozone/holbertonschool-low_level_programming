8fd70be20598fcffe3aa860303e3def27c8254d9
Tnozone <maxnocquet@gmail.com>
2022-10-06 00:47:58 -0700

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * times_table - Entry point
+ * description - adds integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+  return (a + b);
+}
