1d032954e9b7d4d7f1c18d615b03565adddbb9a0
Tnozone <maxnocquet@gmail.com>
2022-10-07 00:32:50 -0700

    most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * print_numbers - Entry point
+ * description - prints numbers 1 - 9 except 2 and 4
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+if (n = 2 || n = 4)
+{
+continue;
+}
+_putchar(n + '0');
+}
+_putchar('\n');
+}
