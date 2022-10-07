d7eea6a7d87995d1957b7d4739c33120ae16221a
Tnozone <maxnocquet@gmail.com>
2022-10-07 07:03:46 -0700

    fibonacci

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -7,17 +7,18 @@
 
 int main(void)
 {
-  int i;
-  int t1 = 1, t2 = 2;
-  int next = t1 + t2;
+int i;
+int t1 = 1, t2 = 2;
+int next = t1 + t2;
 
-  printf("%d, %d\n", t1, t2);
-  for(i = 3; 1 <= 50; ++i)
-    {
-      printf("%d, ", next);
-      t1 = t2;
-      t2 = next;
-      next = t1 + t2;
-    }
-  return (0);
+printf("%d, %d, ", t1, t2);
+for(i = 3; 1 <= 50; ++i)
+{
+printf("%d, ", next);
+t1 = t2;
+t2 = next;
+next = t1 + t2;
+}
+printf("\n");
+return (0);
 }
