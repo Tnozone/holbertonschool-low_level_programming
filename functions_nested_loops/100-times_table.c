ac34b21fb8edd6821b43c0fd8e0e6e465f90815c
Tnozone <maxnocquet@gmail.com>
2022-10-07 04:29:00 -0700

    times table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -15,19 +15,19 @@ for (x = 0; x <= n; x++)
 for (y = 0; y <= n; y++)
 {
 z = x * y;
-if (z <= 9)
+if (z > 99)
 {
-_putchar(z + '0');
+_putchar((z / 100) + '0');
+_putchar((z / 10) + '0');
+_putchar((z % 10) + '0');
 }
-else if (z <= 99)
+else if (z > 9)
 {
 _putchar((z / 10) + '0');
 _putchar((z % 10) + '0');
 }
 else
-_putchar((z / 100) + '0');
-_putchar((z / 10) + '0');                                                                                         
-_putchar((z % 10) + '0'); 
+_putchar(z + '0'); 
 if (y != n)
 {
 _putchar(',');
