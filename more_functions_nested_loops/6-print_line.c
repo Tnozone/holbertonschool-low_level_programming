f7cc5b8a5bf79bdccc559ddcf5cff3efcb52de03
Tnozone <maxnocquet@gmail.com>
2022-10-07 01:21:49 -0700

    line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * print_line - Entry point
+ * description - draws a line
+ * @n: integer for line
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+int l;
+
+if (n > 0)
+{
+for (; l < n; l++)
+{
+_putchar('_');
+}
+}
+_putchar('\n')
+}
