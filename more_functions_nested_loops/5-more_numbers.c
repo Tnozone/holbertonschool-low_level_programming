bed6db758e2c8222cdf58b93e8356b8a18d674c8
Tnozone <maxnocquet@gmail.com>
2022-10-07 00:59:25 -0700

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,8 @@ if (a < 10)
 {
 for (n = 0; n <= 14; n++)
 {
-if (n <= 9)
-{
-_putchar(n + '0');
-}
-else if (n > 9 && n < 15)
-{
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
-}
 }
 _putchar('\n');
 a++;
