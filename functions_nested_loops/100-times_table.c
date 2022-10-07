b4ab8ee0427c70e5dd77f95e432197faf812e3fd
Tnozone <maxnocquet@gmail.com>
2022-10-07 04:23:36 -0700

    times table

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include"main.h"
+/**
+ * print_times_table - Entry point
+ * description - prints a nine times table
+ * @n: number to multiply
+ * Return: Always 0 (Success)
+ */
+
+void print_times_table(int n)
+{
+int x, y, z;
+
+for (x = 0; x <= n; x++)
+{
+for (y = 0; y <= n; y++)
+{
+z = x * y;
+if (z <= 9)
+{
+_putchar(z + '0');
+}
+else if (z <= 99)
+{
+_putchar((z / 10) + '0');
+_putchar((z % 10) + '0');
+}
+else
+_putchar((z / 100) + '0');
+_putchar((z / 10) + '0');                                                                                         
+_putchar((z % 10) + '0'); 
+if (y != n)
+{
+_putchar(',');
+if (z / 10 == 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+}
+else if (z / 100 == 0)
+{
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
+}
+}
