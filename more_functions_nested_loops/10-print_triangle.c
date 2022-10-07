96e69179ed61ac0c67f1786e9862c036b120968e
Tnozone <maxnocquet@gmail.com>
2022-10-07 03:43:06 -0700

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include"main.h"
+/**
+ * print_triangle - Entry point
+ * description - draws a triangle
+ * @size: integer for triangle
+ * Return: Always 0 (Success)
+ */
+
+void print_triangle(int size)
+{
+  int s, h, t;
+
+  if (size > 0)
+    {
+      for (h = 0; h < size; h++)
+	{
+	  for (s = 0; s <= h; s++)
+	    {
+	      for (t = (1 - (size - h)); t < 0; t++)
+		{
+		  _putchar(' ');
+		}
+	      _putchar('#');
+	    }
+	  _putchar('\n');
+	}
+    }
+  else
+    {
+      _putchar('\n');
+    }
+}
