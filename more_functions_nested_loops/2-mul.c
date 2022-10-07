4619914d6f69a3f70b2cdc872dd9233f49ee149a
Tnozone <maxnocquet@gmail.com>
2022-10-07 00:21:16 -0700

    multiply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * mul - Entry point
+ * description - multiplies integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+  return (a * b);
+}
