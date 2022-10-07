4c8338cb4f4cf72ad97548a5971ced36ecf46344
Tnozone <maxnocquet@gmail.com>
2022-10-07 01:57:41 -0700

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * print_square - Entry point
+ * description - draws a square
+ * @size: integer for square
+ * Return: Always 0 (Success)
+ */
+
+void print_square(int size);
+{
+int s, h;
+
+if (size > 0)
+{
+for (s = 0; s < size; s++)
+{
+for (s = 0; s < size; s++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
