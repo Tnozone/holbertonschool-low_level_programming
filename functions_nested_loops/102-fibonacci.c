e13b64833c15ffbfc89eecdf0a7b7e798ab63eda
Tnozone <maxnocquet@gmail.com>
2022-10-07 06:31:42 -0700

    fibonacci

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description - prints fibonacci sequence
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  int i, n;
+  int t1 = 1, t2 = 2;
+  int next = t1 + t2;
+
+  printf("%d, %d\n", t1, t2);
+  for(i = 3; 1 <= 50; ++i)
+    {
+      printf("%d, ", next);
+      t1 = t2;
+      t2 = next;
+      next = t1 + t2;
+    }
+  return (0);
+}
