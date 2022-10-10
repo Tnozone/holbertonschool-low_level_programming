cdc1d53a1d06d21b52e86fc546ab50264b68dca6
Tnozone <maxnocquet@gmail.com>
2022-10-10 00:44:22 -0700

    reset

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * reset_to_98 - Entry point
+ * description - make a pointer point to 98
+ * @n: pointer
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+  int a;
+  int *n = &a;
+  *n = 98;
+}
