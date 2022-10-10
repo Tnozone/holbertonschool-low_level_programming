066ddbf9968598a20609045d9e4a05e1b653a7bb
Tnozone <maxnocquet@gmail.com>
2022-10-10 00:57:18 -0700

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * reset_to_98 - Entry point
+ * description - make a pointer point to 98
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
