5f7ead13709009e02208bc4b97eccbc9e9a935e3
Tnozone <maxnocquet@gmail.com>
2022-10-10 01:23:44 -0700

    string length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _strlen - Entry point
+ * description - make a pointer point to 98
+ * @s: character pointer
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+  return(&s);
+}
