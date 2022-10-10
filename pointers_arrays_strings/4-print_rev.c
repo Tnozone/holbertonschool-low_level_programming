69b0deeb2412292afac1b9f01a5e71375a5f7efc
Tnozone <maxnocquet@gmail.com>
2022-10-10 05:15:15 -0700

    print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * print_rev - Entry point
+ * description - prints a string
+ * @s: pointer to string
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+int end, x;
+
+while (s[end] != '\0')
+{
+end++;
+}
+for (x = end; x >= 0; x--)
+{
+_putchar(*s[x]);
+}
+_putchar('\n');
+}
