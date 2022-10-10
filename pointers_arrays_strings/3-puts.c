97f3b021d16248a0d57570fc34e7b42a64ac9f24
Tnozone <maxnocquet@gmail.com>
2022-10-10 03:42:13 -0700

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * _puts - Entry point
+ * description - prints a string
+ * @str: pointer to string
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+_putchar(*str\0);
+_putchar('\n');
+}
