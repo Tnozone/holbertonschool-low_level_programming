395e0ab12192eb19ed4090343c2d5982bfe3c65a
Tnozone <maxnocquet@gmail.com>
2022-10-11 00:56:13 -0700

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * *_strcat - Entry point
+ * description - cancatenates two strings
+ * @dest: destination buffer
+ * @src: string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int x, y = 0, len = 0;
+
+while (src[len] != '\0')
+{
+len++;
+}
+len--;
+x = (len * 2) + 1;
+  
+while (len <= x)
+{
+dest[len] = src[y];
+y++;
+len++;
+}
+return (dest);
+}
