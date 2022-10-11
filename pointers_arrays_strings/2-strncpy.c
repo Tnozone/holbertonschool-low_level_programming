8981fb0842e6cb2408f3789aef1897faea23bfa0
Tnozone <maxnocquet@gmail.com>
2022-10-11 02:00:47 -0700

    n copy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * *_strncpy - Entry point
+ * description - copies a string
+ * @dest: destination buffer
+ * @src: string
+ * @n: integer
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int y;
+
+for (y = 0; y < n && src[y] != '\0'; y++)
+{
+dest[y] = src[y];
+}
+dest[y] = '\0';
+return (dest);
+}
