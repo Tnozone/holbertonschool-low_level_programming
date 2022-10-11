911f34267d698458ab45a455bc39dd3485855c57
Tnozone <maxnocquet@gmail.com>
2022-10-11 02:23:42 -0700

    cmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * *_strncmp - Entry point
+ * description - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: y
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int y, z = 0, len = 0;
+
+while (s1[len] != '\0')
+{
+len++;
+}
+
+while (s2[z] != '\0')
+{
+z++;
+}
+
+y = len - z;
+return (y);
+}
