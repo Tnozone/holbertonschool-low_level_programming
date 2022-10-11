165e42caf1c2129a8615766d9a3c5ba252a97a01
Tnozone <maxnocquet@gmail.com>
2022-10-11 01:15:35 -0700

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-int y = 0, z = 0 len = 0;
+int y = 0, z = 0, len = 0;
 
 while (src[len] != '\0')
 {
 len++;
 }
 
-while (dst[z] != '\0')
+while (dest[z] != '\0')
 {
 z++;
 }
