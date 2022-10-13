4b860791d435a6abce8b7ea808e0d094ab2f18b0
Tnozone <maxnocquet@gmail.com>
2022-10-13 00:40:00 -0700

    toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * *string_toupper - Entry point
+ * description - prints a string
+ * @s: pointer to string
+ * Return: string
+ */
+
+char *string_toupper(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+if (*s = a-z)
+{
+s = s - 32);
+}
+i++;
+}
+
+return (s);
+}
