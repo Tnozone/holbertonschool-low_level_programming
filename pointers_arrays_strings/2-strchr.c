c4cba777c7565fcde6fdd7faee1ca9051aa7b55e
Tnozone <maxnocquet@gmail.com>
2022-10-13 04:20:02 -0700

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * *_strchr - Entry point
+ * description - searchers string for char c
+ * @s: pointer to string
+ * @c: character
+ * Return: pointer or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+return (s[i])
+}
+i++;
+}
+return (NULL);
+}
