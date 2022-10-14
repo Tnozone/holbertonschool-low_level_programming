ce5e1b79dbd10b38f42a26f3805ffdb991616ab6
Tnozone <maxnocquet@gmail.com>
2022-10-14 04:19:39 -0700

    diagadd

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_diagsums - Entry point
+ * description - print sum of diagonals of a square matrix
+ * @a: pointer to string
+ * @size: integer
+ * Return: sum
+ */
+
+void print_diagsums(int *a, int size)
+{
+int i, sum1 = 0, sum2 = 0;
+
+for (i = 0; i < size; i++)
+{
+sum1 += a[i * (size + 1)];
+sum2 += a[(i +1) * (size - 1)];
+}
+printf("%d, %d\n", sum1, sum2);
+}
