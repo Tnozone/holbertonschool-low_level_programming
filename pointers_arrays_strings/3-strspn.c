378803ce2b6309a88dd9a765bd114f2ac87a2a27
Tnozone <maxnocquet@gmail.com>
2022-10-14 05:59:11 -0700

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,25 +9,25 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    int i, j;
-    int r = 0;
+int i, j;
+int r = 0;
 
-    for (i = 0; s[i] != '\0'; i++)
-      {
-	if (s[i] != ' ')
-	  {
-	    for (j = 0; accept[j] != '\0'; j++)
-	      {
-		if (s[i] == accept[j])
-		  {
-		  r++;
-		  }
-	      }
-	  }
-	else
-	  {
-	  return (r);
-	  }
-      }
-    return (r);
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] != ' ')
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+r++;
+}
+}
+}
+else
+{
+return (r);
+}
+}
+return (r);
 }
