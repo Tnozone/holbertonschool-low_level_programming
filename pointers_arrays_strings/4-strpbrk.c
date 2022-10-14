e0445515fa1710f2fb2ffc0de3a3f90535b04486
Tnozone <maxnocquet@gmail.com>
2022-10-14 02:01:08 -0700

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * *_strpbrk - Entry point
+ * description - searchers string for a set of bytes
+ * @s: pointer to string
+ * @accept: string
+ * Return: pointer or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int x;
+
+while (*s = '\0')
+{
+for (x = 0; accept[x] = '\0'; x++)
+{
+if (*s == accept[x])
+{
+return (s);
+}
+}
+s++;
+}
+return (0);
+}
