58cf773c1c5c783175e4d696db8cc71e9613f1da
Tnozone <maxnocquet@gmail.com>
2022-10-14 04:21:50 -0700

    diagadd

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -15,7 +15,7 @@ int i, sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++)
 {
 sum1 += a[i * (size + 1)];
-sum2 += a[(i +1) * (size - 1)];
+sum2 += a[(i + 1) * (size - 1)];
 }
 printf("%d, %d\n", sum1, sum2);
 }
