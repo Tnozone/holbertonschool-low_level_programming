0be4df4eb30d2c97330b0602cae83656a98db755
Tnozone <maxnocquet@gmail.com>
2022-10-17 04:50:09 -0700

    len recur

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,12 +8,10 @@
 
 int _strlen_recursion(char *s)
 {
-  int l = 0;
-  
-  if (*s != '\0')
-    {
-      l++;
-      _strlen_recursion(s + 1);
-    }
-  return (l);
+
+if (*s == '\0')
+{
+return (0);
+}
+return (1 +  _strlen_recursion(s + 1));
 }
