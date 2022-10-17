ff3a5845f0b2dbb951d835158026d9420bede1fa
Tnozone <maxnocquet@gmail.com>
2022-10-17 00:45:15 -0700

    puts recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _puts_recursion - Entry point
+ * description - compares two strings
+ * @s: string
+ * Return: Sucess
+ */
+
+void _puts_recursion(char *s)
+{
+_putchar(*c); 
+}
+
+int main(void)
+{
+_puts_recursion(*s);
+}
