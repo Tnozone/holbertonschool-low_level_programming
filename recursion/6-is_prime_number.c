1cd58278431a855b8400e5cc414cb68b9cddf1c8
Tnozone <maxnocquet@gmail.com>
2022-10-18 05:40:18 -0700

    prime

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+/**
+ * prime - fonction qui va chercher si n est un nombre premier
+ * @n: variable a verifie
+ * @i: nombre qui teste
+ * Return: si nombre premier (1) sinon (0)
+ */
+
+int prime(int n, int i)
+{
+if (n <= 1 || n % i == 0)
+{
+return (0);
+}
+
+else if (n == i)
+{
+return (1);
+}
+
+else if (n > i)
+{
+prime(n, i + 1);
+}
+
+return (1);
+}
+
+/**
+ * is_prime_number - Entry point
+ * @n: variable a verifie
+ * Return: (prime)
+ */
+
+int is_prime_number(int n)
+{
+return (root(n, 2));
+}
