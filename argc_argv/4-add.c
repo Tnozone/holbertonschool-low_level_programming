c775072a7eeb639677580301c8d8dcb639275877
Tnozone <maxnocquet@gmail.com>
2022-10-20 04:31:16 -0700

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints the number of arguments
+ * @argc: int of argument
+ * @argv: Pointer
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i, j, sum = 0;
+
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[1][j]; j++)
+{
+if (!isdigit(argv[i][j])
+{
+printf("Error\n");
+return (1);
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);	
+return (0);
+}
