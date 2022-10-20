757722bcb915022ce83174d9f32c67acf335579c
Tnozone <maxnocquet@gmail.com>
2022-10-20 03:51:26 -0700

    argv

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program that prints the number of arguments
+ * @argc: int of argument
+ * @argv: Pointer
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+
+for(i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
