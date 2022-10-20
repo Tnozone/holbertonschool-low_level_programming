8ff29213e4af0746b1841983572e139275edc5ef
Tnozone <maxnocquet@gmail.com>
2022-10-20 03:43:38 -0700

    unused attribute

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,10 +2,11 @@
 /**
  * main - program that prints the number of arguments
  * @argc: int of argument
+ * @argv: Pointer
  * Return: 0
  */
 
-int main(int argc)
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 printf("%d", argc);
 return (0);
