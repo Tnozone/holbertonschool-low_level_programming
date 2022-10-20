e66977c3147d66e1d70f3accb6d987be1498100f
Tnozone <maxnocquet@gmail.com>
2022-10-20 00:38:35 -0700

    static lib

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -12,45 +12,35 @@ int x, y, z;
 
 if (n <= 15 && n >= 0)
 {
-for (x = 0; x <= n; x++)
-{
-for (y = 0; y <= n; y++)
-{
-z = x * y;
-if (z > 99)
-{
-_putchar((z / 100) + '0');
-_putchar(((z / 10) % 10) + '0');
-_putchar((z % 10) + '0');
-}
-else if (z > 9)
-{
-_putchar((z / 10) + '0');
-_putchar((z % 10) + '0');
-}
-else
-_putchar(z + '0');
-if (y != n)
-{
-_putchar(',');
-if (z < 10)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-}
-else if (z > 9 && z < 100)
-{
-_putchar(' ');
-_putchar(' ');
-}
-else
-{
-_putchar(' ');
-}
-}
-}
-_putchar('\n');
-}
-}
+  for (x = 0; x <= n; x++)
+    {
+    for (y = 0; y <= n; y++)
+      {
+	z = x * y;
+	if (z > 99)
+	  _putchar((z / 100) + '0');
+	  _putchar(((z / 10) % 10) + '0');
+	  _putchar((z % 10) + '0');
+	else if (z > 9)
+	  _putchar((z / 10) + '0');
+	  _putchar((z % 10) + '0');
+	else
+	  _putchar(z + '0');
+	if (y != n)
+	  {
+	    _putchar(',');
+	    if (z < 10)
+	      _putchar(' ');
+	      _putchar(' ');
+	      _putchar(' ');
+	    else if (z > 9 && z < 100)
+	      _putchar(' ');
+	      _putchar(' ');
+	    else
+	      _putchar(' ');
+	  }
+      }
+  _putchar('\n');
+    }
+ }
 }
