d46521bef62ce11a32f6259250cd8e2aaa7faab4
Tnozone <maxnocquet@gmail.com>
2022-10-20 03:33:59 -0700

    argc

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: entier de type int
+ * @argv: Pointeur
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
