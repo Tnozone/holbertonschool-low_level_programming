e866f7b399be66af55e4aa3e4418d0c1b83d4238
Tnozone <maxnocquet@gmail.com>
2022-10-21 01:55:25 -0700

    palindrome

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include"main.h"
+/**
+ * len - Entry point
+ * description - returns length of string
+ * @s: string
+ * Return: Sucess
+ */
+
+int len(*s)
+{
+
+  if (*s == '\0')
+    {
+      return (0);
+    }
+  return (1 + len(s + 1));
+}
+
+/**
+ * is_palindrome - Entry point
+ * description - checks if string is a palindrome
+ * @s: string
+ * Return: Sucess
+ */
+
+int is_palindrome(char *s)
+{
+  
+  if (len == 0)
+    {
+      return (1);
+    }
+  else if (*s < len / 2)
+    {
+      if (*s == s[len - 1])
+	{
+	  s++;
+	  len--;
+	}
+      else
+	{
+	  return (0);
+	}
+      return (1);
+    }
+}
