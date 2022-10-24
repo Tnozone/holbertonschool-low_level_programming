5a5bb9af9c4a911fc5369155344e2af637514b0e
Tnozone <maxnocquet@gmail.com>
2022-10-24 06:45:09 -0700

    grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,25 +12,36 @@
 
 int **alloc_grid(int width, int height)
 {
-  int *var;
-  var = malloc(sizeof(int) * (width * height));
+  int i, j;
+  int **var;
 
   if (width <= 0 || height <= 0)
     {
       return (NULL);
     }
-  else if (var == NULL)
+  var = (int **)malloc(height * sizeof(int *));
+  
+  if (var == NULL)
     {
       return (NULL);
     }
-  else
+  for (i = 0; i < height; i++)
     {
-      for (i = 0; i < height; i++)
+      var[i] = (int *)malloc(width * sizeof(int));
+
+      if (var[i] == NULL)
 	{
-	  for (j = 0; j < width; j++)
+	  for (j = 0; j < height; j++)
 	    {
-	      ar[i][j] = height[width];
+	    free(var[j]);
 	    }
+	  free(var);
+	  return (NULL);
+	}
+
+      for (j = 0; j < width; j++)
+	{
+	  var[i][j] = 0;
 	}
     }
       return (var);
