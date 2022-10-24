4a213f3c48524236b05309308c2e9f9a56894439
Tnozone <maxnocquet@gmail.com>
2022-10-24 02:13:23 -0700

    betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,25 +12,25 @@
 
 char *create_array(unsigned int size, char c)
 {
-  char *var;
-  unsigned int i;
-  
-  var = malloc(sizeof(c) * size);
- 
-   if (size == 0)
-    {
-      return (NULL);
-    }
-  else if (var == NULL)
-    {
-      return (NULL);
-    }
-  else
-    {
-      for (i = 0; i < size; i++)
-	{
-	  var[i] = c;
-	}
-  return (var);
-    }
+char *var;
+unsigned int i;
+
+var = malloc(sizeof(c) * size);
+
+if (size == 0)
+{
+return (NULL);
+}
+else if (var == NULL)
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+var[i] = c;
+}
+return (var);
+}
 }
