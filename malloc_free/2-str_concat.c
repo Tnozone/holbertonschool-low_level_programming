cf8af5659e86724f9f1d336fae961c96ee97dbed
Tnozone <maxnocquet@gmail.com>
2022-10-24 05:19:38 -0700

    fixes

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
     {
   for(x = 0; s1[x] != '\0'; x++)
     {
-      x++;
+      ;
     }
     }
   if (s2 == NULL)
@@ -34,7 +34,7 @@ char *str_concat(char *s1, char *s2)
     {
   for(y = 0; s2[y] != '\0'; y++)
     {
-      y++;
+      ;
     }
     }
 
