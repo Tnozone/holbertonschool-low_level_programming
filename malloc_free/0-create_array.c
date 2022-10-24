cc1c221b21185228379e44f48415fe4c312174cf
Tnozone <maxnocquet@gmail.com>
2022-10-24 01:02:56 -0700

    create array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - creates an array of chars
+ * @c: specified char
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+  *var = malloc(sizeof(int) + malloc(sizeof(char);
+
+  if (size == 0)
+    {
+      return (0);
+    }
+  while (1)
+    {
+      return (var);
+    }
+  return (0);
+}
