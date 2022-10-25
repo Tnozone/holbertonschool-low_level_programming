7b45079180b6df42d3efb41e8981d281ad94f62f
Tnozone <maxnocquet@gmail.com>
2022-10-25 01:40:10 -0700

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,44 +12,44 @@
 
 char *str_concat(char *s1, char *s2)
 {
-  int x, y, i, s;
-  char *ar;
+int x, y, i, s;
+char *ar;
 
-  if (s1 == NULL)
-    {
-      s1 = "";
-    }
-  for(x = 0; s1[x] != '\0'; x++)
-    {
-      ;
-    }
-  if (s2 == NULL)
-    {
-      s2 = "";
-    }
-  for(y = 0; s2[y] != '\0'; y++)
-    {
-      ;
-    }
+if (s1 == NULL)
+{
+s1 = "";
+}
+for(x = 0; s1[x] != '\0'; x++)
+{
+;
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+for(y = 0; s2[y] != '\0'; y++)
+{
+;
+}
 
-  s = x + y + 1;  
-  ar = malloc(sizeof(char) * s);
+s = x + y + 1;  
+ar = malloc(sizeof(char) * s);
 
 if (ar == NULL)
-    {
-      return (NULL);
-    }
- else
-   {
-     for (i = 0; i < x; i++)
-       {
-	 ar[i] = s1[i];
-       }
-     for (i = 0; i <= y; i++)
-       {
-	 ar[i + x] = s2[i];
-       }
-     ar[x + y] = '\0';
-   }
- return (ar);
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i < x; i++)
+{
+ar[i] = s1[i];
+}
+for (i = 0; i <= y; i++)
+{
+ar[i + x] = s2[i];
+}
+ar[x + y] = '\0';
+}
+return (ar);
 }
