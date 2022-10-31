acc08e4a4b9780809513dc00d0f964fdfc78595b
Tnozone <maxnocquet@gmail.com>
2022-10-31 01:48:26 -0700

    set struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - structure dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Return: Always dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
