aec17a498550e790de944cd01640d7d036f7762d
Tnozone <maxnocquet@gmail.com>
2022-11-03 07:12:21 -0700

    sum

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include<stdarg.h>
+#include"variadic_functions.h"
+
+/**
+ * sum_them_all - adds all parameters
+ * @n: first parameter
+ * Return: sum or 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, r;
+	va_list arg;
+
+	if (n == 0)
+		return (0)
+
+	va_start(arg, n);
+
+	for (i = 1; i < n; i++)
+		r += va_arg(arg, int);
+
+	va_end(arg);
+	return (r);
+}
