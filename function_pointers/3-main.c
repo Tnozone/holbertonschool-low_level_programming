b2f994122a3594c97e92548b5ba76cd9c0ee8a3b
Tnozone <maxnocquet@gmail.com>
2022-11-03 03:42:51 -0700

    testing

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,29 +2,33 @@
 
 /**
  * main  - entry
- *
- *
- * Return: 
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: r
  */
 
-main()
+char main(int *argc, char argv[])
 {
-	atoi(a);
-	atoi(b);
-	if (argc > 4)
+	int r;
+	num1 = atoi(argv[1]);
+	nume 2 = atoi(argv[2]);
+
+	if (argc != 4)
 	{
 		printf("ERROR");
 		exit (98);
 	}
-	if (operator != '+' && operator != '-' &&
-		operator != '*' && operator != '/' && operator != '%')
+	if (s == NULL)
 	{
 		printf("ERROR");
 		exit (99);
 	}
-	if ((operator == '/' || operator == %) && b == 0)
+	if ((operator == '/' || operator == %) && num2 == 0)
 	{
 		prinf("ERROR");
 		exit (100);
 	}
+
+	r = get_op_func(*s)(num1, num2);
+	return (r);
 }
