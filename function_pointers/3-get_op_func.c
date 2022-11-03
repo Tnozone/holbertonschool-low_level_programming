2c3dc21129c9dae7ddea2a7db0e8bb4182ed3c9d
Tnozone <maxnocquet@gmail.com>
2022-11-03 04:18:18 -0700

    corrections 2

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -17,13 +17,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 		};
-	int i;
+	int i = 0;
 
 	while (ops[i].op != NULL)
 	{
-		if (ops[i].op == s)
+		if (*ops[i].op == *s)
 			return (ops[i].f);
-		
+		i++;	
 	}
 
 	return (NULL);
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,26 +10,32 @@
 int main(int argc, char *argv[])
 {
 	int r;
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
+	int num1;
+	int num2;
+	char *s;
 
 	if (argc != 4)
 	{
 		printf("ERROR");
 		exit (98);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	s = (argv[2]);
+
 	if (s == NULL)
 	{
 		printf("ERROR");
 		exit (99);
 	}
-	if ((op_s == '/' || op_s == '%') && num2 == 0)
+	if ((*s == '/' || *s == '%') && num2 == 0)
 	{
 		printf("ERROR");
 		exit (100);
 	}
 
-	r = *get_op_func(*s)(num1, num2);
+	r = get_op_func(s)(num1, num2);
 	printf("%d\n", r);
 	return (0);
 }
