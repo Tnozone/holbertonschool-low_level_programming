2b9aae3d1ea55f04b2f6b1ae147dfe7bf03c5b58
Tnozone <maxnocquet@gmail.com>
2022-11-04 05:51:09 -0700

    3 structure

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,6 +3,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+ * struct unit - structure format
+ * @c: char
+ * @i: int
+ * @f: float
+ * @s: string
+ * longer description
+ */
+
+typedef struct unit
+{
+	char c;
+	int i;
+	float f;
+	char *s;
+} unit;
+
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
