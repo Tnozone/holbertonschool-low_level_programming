fa5c95e0225828a73f2bd6c049e8cdcac50af69b
Tnozone <maxnocquet@gmail.com>
2022-11-04 05:53:30 -0700

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,22 +2,12 @@
 #include"variadic_functions.h"
 
 /**
- * unit - structure
- */
-
-struct unit{char c;
-	int i;
-	float f;
-	char *s;
-	    };
-
-/**
  * print_all - prints anything
  * @format: the format
  * Return: 0
  */
 
-void print_all(const char * const format, ...);
+void print_all(const char * const format, ...)
 {
 	int inc = 0;
 	va_list arg;
