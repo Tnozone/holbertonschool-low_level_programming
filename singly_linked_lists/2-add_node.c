71c6f4a2e47b89c44ae1d2f639bf00421b5c89f6
Tnozone <maxnocquet@gmail.com>
2022-11-14 04:56:54 -0800

    add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node at the beginning of a list
+ * @head: head node
+ * @str: string
+ * Return: address of the new element or NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	new = (list_t *) malloc(sizeof(list_t);
+
+	new->str = *str;
+	new->next = head;
+	head = new;
+	return (**head);
+}
