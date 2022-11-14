c93d21fda54fe1c5e51dc45890eed176d725c536
Tnozone <maxnocquet@gmail.com>
2022-11-14 05:42:49 -0800

    variables

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,10 +10,28 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	char *newstr;
+	int x = 0;
+
 	new = malloc(sizeof(list_t));
 
-	new->str = strdup(str);
+	if (new == NULL)
+		return (NULL);
+
+	newstr = strdup(str);
+	if (newstr == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	while (str[x])
+		x++;
+
+	new->str = newstr;
+	new->len = x;
 	new->next = *head;
 	*head = new;
-	return (*head);
+
+	return (new);
 }
