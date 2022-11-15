d15b32da1fa550e81e7b052dace2d5acbcb353bb
Tnozone <maxnocquet@gmail.com>
2022-11-15 04:57:11 -0800

    rm if else

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -12,10 +12,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (h->n == NULL)
-			printf("Error\n");
-		else
-			printf("%d\n", h->n);
+		printf("%d\n", h->n);
 		x++;
 		h = h->next;
 	}
