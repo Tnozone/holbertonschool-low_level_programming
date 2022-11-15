10b09cb435b9d86f0ab2ab480e53ac6760c2284c
Tnozone <maxnocquet@gmail.com>
2022-11-15 05:09:49 -0800

    list len

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *listint_len - finds the number of elements in a linked list
+ *@h: head node
+ *Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int x = 0;
+
+	while (h != NULL)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x)
+}
