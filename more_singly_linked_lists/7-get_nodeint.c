518aa7ff938817525167ecbd6b5636055cddeb28
Tnozone <maxnocquet@gmail.com>
2022-11-17 01:37:58 -0800

    for loop

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -9,16 +9,15 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < index)
+	for (i = 0; i < index; i++)
 	{
-		head = head->next;
-		i++;
+		if (head == NULL)
+			return (NULL);
+		else
+			head = head->next;
 	}
 
-	if (head == NULL)
-		return (NULL);
-	else
-		return (head);
+	return (head);
 }
