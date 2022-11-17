14ec2f8979440386c7d1f2e935dfd4382095a19f
Tnozone <maxnocquet@gmail.com>
2022-11-17 02:59:56 -0800

    tmp

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
-	listint_t *tmp
+	listint_t *tmp;
 	unsigned int i;
 
 	new = malloc(sizeof(listint_t));
