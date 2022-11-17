d1a2a5d9d2782ebaeef06cc6d70f7cbbcdd8db17
Tnozone <maxnocquet@gmail.com>
2022-11-17 02:13:14 -0800

    add node idx

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at the index
+ * @h: head node
+ * @idx: place to insert node
+ * @n: data
+ * Return: adress of new node or NULL
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new;
+	unsigned int i;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; i < idx; i++)
+	{
+		if (*head == NULL)
+			return (NULL);
+
+		*head = (*head)->next;
+	}
+
+	new->n = n;
+	new->next = (*head)->next;
+	*head = new;
+
+	return (new);
+}
