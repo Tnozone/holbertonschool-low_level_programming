85b3964762c574e52f19cfb5902f4cf0f7056c6c
Tnozone <maxnocquet@gmail.com>
2022-11-18 04:25:28 -0800

    xor

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -9,13 +9,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int mask; 
-	unsigned int x;
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	mask = 1 << index;
-	x = (n & ~mask) | (1 << index);
+	*n ^= (1 << index);
 	return (1);
 }
