8015768bfa4cbb8ed0a07fcce9bbf2fa0adc1e66
Tnozone <maxnocquet@gmail.com>
2022-11-18 05:02:17 -0800

    flip

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * flip_bits - returns number of bits flipped
+ * @n: first bit number
+ * @m: second bit number
+ * Return: number of flips
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flip = n ^ m, bits = 0;
+
+	while (flip > 0)
+	{
+		bits += (flip & 1);
+		flip >>= 1;
+	}
+	return (bits);
+}
