b857095d06fbd67d14fcbf3ec2d94fad1fbcea61
Tnozone <maxnocquet@gmail.com>
2022-11-18 01:33:05 -0800

    int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -8,14 +8,15 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int x = 0, y = 0;
+	int x;
+	unsigned y = 0;
 	unsigned int mul = 1;
 
 	if (b == NULL)
 		return (0);
 
-	while (*b)
-		x++;
+	for (x = 0; b[x]; x++)
+		;
 	x--;
 
 	for (; x >= 0; x--)
