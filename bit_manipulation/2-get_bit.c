fb594e1e1b240992b5da758a0898b4631c570da7
Tnozone <maxnocquet@gmail.com>
2022-11-18 03:51:51 -0800

    get bit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - return bit at index
+ * @n: bit number
+ * @index: position of bit
+ * Return: bit or -1
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int x;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	x = (n << index) & 1;
+	
+	return (x);
+}
