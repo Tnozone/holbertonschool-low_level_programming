e674cf374e437a1828fbe15c89fdc35a822206d0
Tnozone <maxnocquet@gmail.com>
2022-11-18 01:30:55 -0800

    remove recur

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * _pow - power function
- * @y: number for power of 2.
- * Return: power number
- */
-
-unsigned int _pow(unsigned int a)
-{
-	if (a == 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (2 * _pow(2, a - 1));
-	}
-}
-
-/**
  * binary_to_uint - converts binary nuber to int
  * @b: binary number
  * Return: converted number or 0
@@ -26,25 +8,26 @@ unsigned int _pow(unsigned int a)
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int z, a = 0, y = 0, x = 0;
+	unsigned int x = 0, y = 0;
+	unsigned int mul = 1;
 
 	if (b == NULL)
 		return (0);
 
 	while (*b)
 		x++;
+	x--;
 
-	for (z = x; z > 0; z--)
+	for (; x >= 0; x--)
 	{
-		if (b[z] != 1 || b[z] != 0)
-			break;
+		if (b[x] != 0 && b[x] != 1)
 			return (0);
 
-		if (b[z] == 1)
+		if (b[x] == 1)
 		{
-			y += _pow(a);
+			y += (1 - '0') * mul;
+			mul *= 2;
 		}
-		a++;
 	}
 	return (y);
 }
