8a0ac29ea324206525a46e2982059162c03f077a
Tnozone <maxnocquet@gmail.com>
2022-11-18 00:50:56 -0800

    binary convert

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * pow - power function
+ * @y: number for power of 2.
+ * Return: power number
+ */
+
+int pow(int y)
+{
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (2 * pow(2, y - 1));
+	}
+}
+
+/**
+ * binary_to_uint - converts binary nuber to int
+ * @b: binary number
+ * Return: converted number or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int a = 0, y = 0, x = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+		x++;
+
+	for (x; x >= 0, x--)
+	{
+		if (b[x] != 1 || b[x] != 0)
+			return (0);
+
+		if (b[x] == 1)
+		{
+			y += pow(a);
+		}
+		a++;
+	}
+	return (y);
+}
