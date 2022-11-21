c220284dea2ff4fc9fa149df010ac7c95d2ec21d
Tnozone <maxnocquet@gmail.com>
2022-11-21 04:57:05 -0800

    betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,10 +21,10 @@ int create_file(const char *filename, char *text_content)
 			;
 	}
 
-	txt = malloc (sizeof(char) * x);
+	txt = malloc(sizeof(char) * x);
 
 	op = open(filename, O_CREAT | O_RDWR, 0600);
-	
+
 	if (text_content != NULL)
 		wr = write(op, text_content, x);
 
