572b636f4b7906be4f1887ae5dfdb3fb9ba9b7c3
Tnozone <maxnocquet@gmail.com>
2022-11-21 07:05:23 -0800

    shorten code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,16 +14,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	if (text_content != NULL)
 	{
 		for (x = 0; text_content[x] != '\0'; x++)
 			;
-	}
-
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (text_content != NULL)
 		wr = write(op, text_content, x);
+	}
 
 	if (op == -1 || wr == -1)
 		return (-1);
