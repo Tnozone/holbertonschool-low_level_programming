0c0af2295b9b35266b3bb8d4b588853b422832d9
Tnozone <maxnocquet@gmail.com>
2022-11-21 05:09:47 -0800

    no malloc

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,7 +10,6 @@
 int create_file(const char *filename, char *text_content)
 {
 	int op, wr, x;
-	char *txt;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,18 +20,13 @@ int create_file(const char *filename, char *text_content)
 			;
 	}
 
-	txt = malloc(sizeof(char) * x);
-
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (text_content != NULL)
 		wr = write(op, text_content, x);
 
 	if (op == -1 || wr == -1)
-	{
-		free(txt);
 		return (-1);
-	}
 
 	close(op);
 	return (1);
