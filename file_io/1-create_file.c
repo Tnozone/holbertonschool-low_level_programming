ef6a2e28ff3d4ca250fd8d0f6f5c941d4bf8ba3c
Tnozone <maxnocquet@gmail.com>
2022-11-21 05:08:11 -0800

    o_trunc

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 
 	txt = malloc(sizeof(char) * x);
 
-	op = open(filename, O_CREAT | O_RDWR, 0600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (text_content != NULL)
 		wr = write(op, text_content, x);
