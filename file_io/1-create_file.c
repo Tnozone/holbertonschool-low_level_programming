aa2c6ce7a782360ac34ae3494ccd196ea4386931
Tnozone <maxnocquet@gmail.com>
2022-11-21 04:34:17 -0800

    create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file to create
+ * @text_content: file content
+ * Return: 1 or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op;
+	int x = 0;
+	char *txt;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content[x] != '\0')
+		x++;
+
+	txt = malloc (sizeof(char) * x);
+
+	op = open(filename, O_CREAT | O_RDWR, 0600);
+	
+	if (text_content != NULL)
+		wr = write(op, text_content, txt);
+
+	if (op == -1 || wr == -1)
+	{
+		free(txt);
+		return (-1);
+	}
+
+	close(op);
+	return (1);
+}
