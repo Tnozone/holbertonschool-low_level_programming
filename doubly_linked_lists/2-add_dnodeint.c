7e6cf331ebc3fe172bb65ccd940811a914d56cf0
Tnozone <maxnocquet@gmail.com>
2022-11-22 03:33:28 -0800

    kernel

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,22 +1,24 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - add new node at the beginning of the list
+ * *add_dnodeint - add new node at the beginning of the list
  * @head: head node
  * @n: data
- * Return: 
+ * Return: adress of new node, or NUll
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+	int m;
 
 	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
 		return (NULL);
 
-	new->n = n;
+	m = n;
+	new->n = m;
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
