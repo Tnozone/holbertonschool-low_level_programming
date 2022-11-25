ad738967f2a88ca60df7f627a80287a116070074
Tnozone <maxnocquet@gmail.com>
2022-11-25 04:13:13 -0800

    delete hash

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete hash table
+ * @ht: hash table
+ * Return: 0
+ */
+
+void hash_table_delete(hash_table_t *ht);
+{
+	hash_node_t *node;
+	hash_node_t *tmp;
+	unsigned long int x;
+
+	for (x = 0; ht->size; x++)
+	{
+		if (ht->array[x] != NULL)
+		{
+			node = ht->array[x];
+			while (node != NULL)
+			{
+				tmp = node;
+				node = node->next;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+			}
+			if (x > 0)
+				free(ht->array[x - 1]);
+		}
+	}
+}
