b53007692cf0acb7c60f90a405b67dc782cf869a
Tnozone <maxnocquet@gmail.com>
2022-11-25 04:19:16 -0800

    size

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -12,7 +12,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *tmp;
 	unsigned long int x;
 
-	for (x = 0; ht->size; x++)
+	for (x = 0; x < ht->size; x++)
 	{
 		if (ht->array[x] != NULL)
 		{
