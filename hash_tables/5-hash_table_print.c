460578e6160dae5b065ebca327353566de5c41ae
Tnozone <maxnocquet@gmail.com>
2022-11-25 02:28:23 -0800

    print format

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,18 +10,26 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node = NULL;
 	unsigned long int x;
+	char com = 0;
 
 	if (ht == NULL)
 		return;
 
+	printf("{");
 	for (x = 0; ht->size; x++)
 	{
+		if (com == 1)
+			printf(", ");
 		node = ht->array[x];
 
 		while (node)
 		{
-			printf("\"%s\", \"%s\"", (char *) node->key, (char *) node->value);
+			printf("'%s': '%s'", node->key, node->value);
 			node = node->next;
+			if (node != NULL)
+				printf(", ");
 		}
+		com = 1;
 	}
+	printf("}\n");
 }
