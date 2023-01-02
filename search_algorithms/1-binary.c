c0800cfa3b4d229cddf27fc82e2af969e5964d7e
Tnozone <maxnocquet@gmail.com>
2023-01-02 06:03:04 -0800

    binary search

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - binary search algorithm
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search
+ * Retunr: index of value, or -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int min, max, med, x;
+
+	if (array == NULL)
+		return (-1);
+
+	min = 0;
+	max = size;
+
+	while (min <= max)
+	{
+		med = min + (max - min) / 2;
+
+		printf("Searching in array: ");
+		for (x = min; x < max; x++)
+			printf("%d, ", array[x]);
+		printf("%d\n", array[max]);
+
+		if (array[med] == value)
+			return (med);
+		else if (value > array[med])
+			min = med + 1;
+		else
+			max = med - 1;
+	}
+	return (-1);
+}
