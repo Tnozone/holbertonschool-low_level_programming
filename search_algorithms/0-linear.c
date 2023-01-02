489bf5948f70eb9bda8c4a07601c05f0925ed0aa
Tnozone <maxnocquet@gmail.com>
2023-01-02 01:51:40 -0800

    printf zd

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (x = 0; x < size; x++)
 	{
-		printf("Value checked array[%d] = [%d]", x, array[x]);
+		printf("Value checked array[%zd] = [%d]", x, array[x]);
 		if (array[x] == value)
 			return (x);
 	}
