3f830bc928603f650602dddee81e730751a95d28
Tnozone <maxnocquet@gmail.com>
2023-01-02 07:02:16 -0800

    mid mod

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -20,7 +20,7 @@ int binary_search(int *array, size_t size, int value)
 
 	while (min <= max)
 	{
-		med = min + (max - min) / 2;
+		med = (min + max) / 2;
 
 		printf("Searching in array: %d", array[min]);
 		for (x = min + 1; x < max; x++)
