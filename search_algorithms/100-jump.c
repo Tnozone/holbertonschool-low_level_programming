3325fb95c329fcdec96df565e3cc24a87b67a18d
Tnozone <maxnocquet@gmail.com>
2023-01-03 11:21:31 -0800

    jump search

---
+++ search_algorithms/100-jump.c
@@ -0,0 +1,54 @@
+#include "search_algos.h"
+
+/**
+ * min - function
+ * @a: first value
+ * @b: second value
+ * Return: a or b
+ */
+
+int min(int a, int b)
+{
+	if (b > a)
+		return a;
+	else
+		return b;
+}
+
+/**
+ * jump_search - jump search algorithm
+ * @array: array to search
+ * @size: array size
+ * @value: value to find
+ * Return: index of value, or -1
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	int s = size;
+	int step = sqrt(s);
+	int prev = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (array[min(step, s) - 1] < value)
+	{
+		prev = step;
+		step += sqrt(s);
+		if (prev >= s)
+			return (-1)
+	}
+
+	while (array[prev] <= value)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (prev == min(step, s))
+			return (-1);
+		if (array[prev] == value)
+			return (prev);
+		prev++;
+	}
+
+	return (-1);
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -2,8 +2,11 @@
 #define _SEARCH_ALGOS_H_
 
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int min(int a, int b);
+int jump_search(int *array, size_t size, int value);
 
 #endif
