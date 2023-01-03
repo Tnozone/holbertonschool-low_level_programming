add8bea2d979a69cd7cd34af724fd2253baac6c9
Tnozone <maxnocquet@gmail.com>
2023-01-03 11:26:00 -0800

    added more printf

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -10,9 +10,9 @@
 int min(int a, int b)
 {
 	if (b > a)
-		return a;
+		return (a);
 	else
-		return b;
+		return (b);
 }
 
 /**
@@ -34,6 +34,7 @@ int jump_search(int *array, size_t size, int value)
 
 	while (array[min(step, s) - 1] < value)
 	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 		prev = step;
 		step += sqrt(s);
 		if (prev >= s)
