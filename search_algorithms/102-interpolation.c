e48dc17fde75a7623d4eb41cc77441f7dfea8c4f
Tnozone <maxnocquet@gmail.com>
2023-01-06 00:26:07 -0800

    interpolation search

---
+++ search_algorithms/102-interpolation.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - interpolation search algorithm
+ * @array: array to search
+ * @size: range size
+ * @value: value to find
+ * Return: index of value, or -1
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	int pos;
+	int low = 0;
+	int high = size - 1;
+
+	while (low <= high)
+	{
+		pos = low
+			+ (((double)(high - low) / (array[high] - array [low]))
+				* (x - array[low]));
+	
+		printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
+		if (array[pos] == x)
+			return (pos);
+
+		if (array[pos] < x)
+			low = pos + 1;
+		if (array[pos] > x)
+			high = pos - 1;
+	}
+	return (-1);
+
+}
